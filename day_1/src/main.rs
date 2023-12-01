e75d1897380a6e6627b838f35248e271837cbeb4
colemalphrus <colemalphrus@gmail.com>
2023-12-01 02:36:07 -0500

    Part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,17 +1,93 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+//fn get_num(input: &str) -> Option<i32> {
+//    let s = input.to_string();
+//    let digits: Vec<char> = s.chars().filter(|c| c.is_digit(10)).collect();
+//
+//    if digits.is_empty() {
+//        None
+//    } else {
+//        let first = digits.first().unwrap();
+//        let last = digits.last().unwrap();
+//        // println!("{}{}", first, last);
+//        format!("{}{}", first, last).parse::<i32>().ok()
+//    }
+//}
 
-fn get_num(input: &str) -> Option<i32> {
-    let digits: Vec<char> = input.chars().filter(|c| c.is_digit(10)).collect();
+fn text_num(s: &str) -> Option<i32> {
+    let mut v: Option<i32> = None;
+    let mut map = HashMap::new();
 
-    if digits.is_empty() {
-        None
-    } else {
-        let first = digits.first().unwrap();
-        let last = digits.last().unwrap();
-        format!("{}{}", first, last).parse::<i32>().ok()
+    map.insert("one", 1);
+    map.insert("two", 2);
+    map.insert("three", 3);
+    map.insert("four", 4);
+    map.insert("five", 5);
+    map.insert("six", 6);
+    map.insert("seven", 7);
+    map.insert("eight", 8);
+    map.insert("nine", 9);
+
+    for (key, value) in &map {
+        match s.find(key) {
+            Some(_) => {
+                v = Some(*value as i32);
+                println!("FOUND::{} {}", s, value);
+                break;
+            }
+            _ => continue,
+        }
+    }
+
+    v
+}
+
+fn get_num2(input: &str) -> Option<i32> {
+    let s = input.to_string();
+    let mut first: Option<i32> = None;
+    let mut last: Option<i32> = None;
+
+    for (index, ch) in s.char_indices() {
+        if ch.is_digit(10) {
+            first = ch.to_string().parse::<i32>().ok();
+            break;
+        } else {
+            let slice = &s[0..index + 1];
+            //println!("{}", slice);
+            match text_num(slice) {
+                Some(n) => {
+                    first = Some(n);
+                    break;
+                }
+                None => continue,
+            }
+        }
+    }
+
+    for (index, ch) in s.char_indices().rev() {
+        if ch.is_digit(10) {
+            last = ch.to_string().parse::<i32>().ok();
+            break;
+        } else {
+            let slice = &s[index..];
+            //println!("{} {}", slice, index);
+            match text_num(slice) {
+                Some(n) => {
+                    last = Some(n);
+                    break;
+                }
+                None => continue,
+            }
+        }
+    }
+
+    //println!("{} {} {}", first.unwrap(), last.unwrap(), s);
+    match (first, last) {
+        (Some(f), Some(l)) => format!("{}{}", f, l).parse::<i32>().ok(),
+        _ => None,
     }
 }
 
@@ -24,7 +100,6 @@ where
 }
 
 fn main() -> io::Result<()> {
-    // Specify the file path here. Change "path/to/file.txt" to your file's path
     let file_path = "src/data.txt";
 
     let lines = read_lines(file_path)?;
@@ -33,16 +108,15 @@ fn main() -> io::Result<()> {
 
     for line in lines {
         if let Ok(ip) = line {
-          match get_num(&ip) {
-            Some(num) => numbers.push(num),
-              None => println!("oops")
-          }
+            match get_num2(&ip) {
+                Some(num) => numbers.push(num),
+                None => println!("oops"),
+            }
         }
     }
 
     let sum: i32 = numbers.iter().sum();
-    println!("{}", sum);
+    println!("ANSWER::{}", sum);
 
     Ok(())
 }
-
